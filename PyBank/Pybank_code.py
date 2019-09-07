fd1ea1e0a60387afbaa8ba214a7d064e011dd64d
ccarr115 <carrillo.carolina1001@gmail.com>
2019-09-06 20:22:32 -0400

    update

--- PyBank/Pybank_code.py
+++ PyBank/Pybank_code.py
@@ -1,63 +1,76 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+# import os and csv modules
 import os
-
-# Module for reading CSV files
 import csv
 
-pybank_csvpath = os.path.join('PyBank/Budget_data.csv')
-with open(pybank_csvpath, newline='') as csvfile:
+# connect to data
+Budget_data = os.path.join('..', 'PyBank', 'Budget_data.csv')
+
+with open(Budget_data, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-file_output = "pybank_results.txt"
-total_months = 0
-total_revenue = 0
-months_of_change =  []
-previous_revenue = 0
-revenue_change_list = []
-greatest_increase = ["",0]
-greatest_decrease = ["",99999999999]
-with open(pybank_csvpath,newline="") as csvfile:  
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        total_months = total_months + 1
-        total_revenue = total_revenue + int(row["Revenue"])
-        previous_revenue = int(row["Revenue"])
-        revenue_change = int(row["Revenue"])- previous_revenue
-        revenue_change_list = revenue_change_list + [revenue_change]
-        month_of_change = month_of_change + [row["Date"]]
-if revenue_change <greatest_decrease[1]:
-    greatest_decrease[1]= revenue_change
-    greatest_decrease[0] = row['Date']
+    csv_header = next(csvreader)
 
-#print(rev_change_list)
-rev_avg = sum(revenue_change_list)/len(revenue_change_list)
+    rows_count = 0
+    net_profit = 0
+    month_list = []
+    profit_list = []
+    monthly_revenue_changes = []
 
+#  calculate total number of months included in the dataset
+    # loop through each row
+    for row in csvreader:
+        # add 1 for each row in loop (total # of row = total months)
+        rows_count = rows_count + 1
 
-print('Average Change in Revenue: $ ' + str(rev_avg))
-print("Total Months: " + str(total_months))
-print("Total Revenue: $ " + str(total_revenue))
-print(greatest_increase)
-print(greatest_decrease)
+# calculate net total amount of "Profit/Losses" over the entire period
+        # sum column 2
+        net_profit = net_profit + int(row[1])
 
-if revenue_change<greatest_decrease[1]:
-    greatest_decrease[1]= revenue_change
-    greatest_decrease[0] = row['Date']
+# calculate average of the changes in "Profit/Losses" over the entire period
 
-#print(rev_change_list)
-rev_avg = sum(revenue_change_list)/len(revenue_change_list)
+        # append all of the values to make an indexed list
+        profit_list.append(row[1])
+        month_list.append(row[0])
 
+    for i in range(1,len(profit_list)):
+        # calculate difference in profit values between rows, and populate monthly rev change list with those values
+        monthly_revenue_changes.append(int(profit_list[i]) - int(profit_list[i-1]))   
+        # calculate mean by summing the list and then dividing by it's length
+        avg_rev_change = sum(monthly_revenue_changes)/len(monthly_revenue_changes)
 
-print('Average Change in Revenue: $ ' + str(rev_avg))
-print("Total Months: " + str(total_months))
-print("Total Revenue: $ " + str(total_revenue))
-print(greatest_increase)
-print(greatest_decrease)
+# identify the greatest increase in profits (date and amount) over the entire period
+#    
+        max_rev_change = max(monthly_revenue_changes)
+        max_index = monthly_revenue_changes.index(max_rev_change)
+        max_month = month_list[max_index + 1]
 
-with open(file_output, 'w') as file:
-    file.write("Financial Analysis\n")
-    file.write("---------------------\n")
-    file.write("Total Months: %d\n" % total_months)
-    file.write("Total Revenue: $%d\n" % total_revenue)
-    file.write("Average Revenue Change $%d\n" % rev_avg)
-    file.write("Greatest Increase in Revenue: %s ($%s)\n" % (greatest_increase[0], greatest_increase[1]))
-    file.write("Greatest Decrease in Revenue: %s ($%s)\n" % (greatest_decrease[0], greatest_decrease[1]))
+# identify the greatest decrease in losses (date and amount) over the entire period       
+       
+        # now do all of the same steps for the min values
+        min_rev_change = min(monthly_revenue_changes)
+        min_index = monthly_revenue_changes.index(min_rev_change)
+        min_month = month_list[min_index + 1]
+
+# print analysis summary to terminal  
+  
+    print("Financial Analysis")
+    print("-------------------------------------------------------------------")
+    print(f"Total Months:{rows_count}")
+    print(f"Total: ${net_profit}")
+    # the :.2F code tells python to limit the number (a float, see above) to just 2 decimal places
+    print(f"Average Change: ${avg_rev_change:.2F}")
+    print(f"Greatest Increase in Profits: {max_month} (${max_rev_change})")
+    print(f"Greatest Decrease in Profits: {min_month} (${min_rev_change})")
+
+# ---------------------------------------------------------------------------
+# TASK #7 export results to a text file
+# ---------------------------------------------------------------------------
+
+file = open("PyBankResults.txt", "w+")
+
+file.write("Financial Analysis\n")
+file.write("-------------------------------------------------------------------\n")
+file.write(f'Total Months: {rows_count}\n')
+file.write(f'Total: ${net_profit}\n')
+file.write(f'Average Change: ${avg_rev_change:.2F}\n')
+file.write(f'Greatest Increase in Profits: {max_month} (${max_rev_change})\n')
+file.write(f'Greatest Decrease in Profits: {min_month} (${min_rev_change})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,84 @@
+# import os and csv modules
+import os
+import csv
+import operator
 
+election_data = os.path.join('..', 'PyPoll', 'Election_Data.csv')
+
+with open(election_data, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    total_votes = 0
+    Khan_total = 0
+    Correy_total = 0
+    Li_total = 0
+    OTooley_total = 0
+    unduplicated_candidates = set()
+    
+    # calculate total number of votes cast
+
+    for row in csvreader:
+        total_votes = total_votes + 1
+
+    # generate list unique list of candidates
+        unduplicated_candidates.add(row[2]) 
+
+    # number of votes each candidate won
+
+        # create a conditional that looks for an index with each candidate name 
+        if row[2] == "Khan":
+            # increment vote count
+            Khan_total = Khan_total +1
+        
+        elif row[2] == "Correy":
+            Correy_total = Correy_total +1
+
+        elif row[2] == "Li":
+            Li_total = Li_total +1
+
+        elif row[2] == "O'Tooley":
+            OTooley_total = OTooley_total +1
+
+    # percentage of votes each candidate won
+        Khan_precent = (Khan_total / total_votes) * 100
+        Correy_percent = (Correy_total / total_votes) * 100
+        Li_percent = (Li_total / total_votes) * 100
+        OTooley_percent = (OTooley_total / total_votes) * 100
+
+    # identify the winner of the election
+        election_dict = {
+                        "Khan": [Khan_precent, Khan_total],
+                        "Correy": [Correy_percent, Correy_total],
+                        "Li": [Li_percent, Li_total],
+                        "O'Tooley": [OTooley_percent, OTooley_total]
+                        }
+        winner = max(election_dict.items(), key=operator.itemgetter(1))[0]
+ 
+    #  print election results to terminal  
+
+    print("Election Results\n")
+    print("---------------------------------------------------\n")
+    print(f"Total Votes: {total_votes}\n")
+    print("---------------------------------------------------\n")
+    print(f'Khan: {Khan_precent:.3f}% {Khan_total}\n')
+    print(f'Correy: {Correy_percent:.3f}% {Correy_total}\n')
+    print(f'Li: {Li_percent:.3f}% {Li_total}\n')
+    print(f"O'Tooley: {OTooley_percent:.3f}% {OTooley_total}\n")
+    print("---------------------------------------------------\n")
+    print(f'Winner: {winner}\n')
+
+    #  print election results to new txt file  
+
+    file = open("PyPollResults.txt", "w+")
+    
+    file.write("Election Results\n")
+    file.write("---------------------------------------------------\n")
+    file.write(f'Total Votes: {total_votes}\n')
+    file.write("---------------------------------------------------\n")
+    file.write(f'Khan: {Khan_precent:.3f}% ({Khan_total})\n')
+    file.write(f'Correy: {Correy_percent:.3f}% ({Correy_total})\n')
+    file.write(f'Li: {Li_percent:.3f}% ({Li_total})\n')
+    file.write(f"O'Tooley: {OTooley_percent:.3f}% ({OTooley_total})\n")
+    file.write("---------------------------------------------------\n")
+    file.write(f'Winner: {winner}\n')
